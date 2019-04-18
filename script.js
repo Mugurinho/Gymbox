c8fa58f4bafb895c7d8508197d1684d406524864
Mugurinho <mugurel.budara@gmail.com>
2019-04-18 22:37:19 +0100

    removed the var checkbox from the for statement

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ var checkbox = "<input type='checkbox' id='chk'>";
 //4 days a week
 for (var i = 0; i < days.length; i++) {
   workout += "<div class='row'><div class='column'><div class='card'>" + "<h3>" + days[i][0] + "</h3>" + "<hr id='hr1'>" + "<h4>" + 
-  groups[i][0] + "</h4>" + "<hr>" + "<p>" + checkbox + exercises[i][0] + "<p>" + exercises[i][1] + "<p>" + exercises[i][2] + "<p>" + exercises[i][3] + 
+  groups[i][0] + "</h4>" + "<hr>" + "<p>" + exercises[i][0] + "<p>" + exercises[i][1] + "<p>" + exercises[i][2] + "<p>" + exercises[i][3] + 
   "<p>" + exercises[i][4] + "<p>" + exercises[i][5] + "<p>" + exercises[i][6] + "<p>" + exercises[i][7] + "</p>" + 
   "</div></div></div>";
 };
